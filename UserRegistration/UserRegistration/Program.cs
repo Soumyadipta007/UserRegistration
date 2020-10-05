cee6bb2ad0c477e1fad281e3c6a8e3e1631150f3
Soumya <soumyadipta0077@gmail.com>
2020-10-05 12:28:51 +0530

    [Soumya] Add: Validate First Name

---
+++ UserRegistration/UserRegistration/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO.Pipes;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Name");
+            string name = Console.ReadLine();
+            RegexClass regexClass = new RegexClass();
+            if (regexClass.validateFirstName(name))
+                Console.WriteLine("Valid First Name");
+            else
+                Console.WriteLine("InValid First Name");
+            
+        }
+    }
+}
---
+++ UserRegistration/UserRegistration/RegexClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration
+{
+    class RegexClass
+    {
+        public static string NAMEREGEX = "^[A-Z]{1}[A-Za-z]{6,}$";
+        public bool validateFirstName(string name)
+        {
+            return Regex.IsMatch(name, NAMEREGEX);
+        }
+    }
+}
