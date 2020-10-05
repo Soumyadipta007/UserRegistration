06108162a1f47e683cd0d8b31605a52de5fa21df
Soumya <soumyadipta0077@gmail.com>
2020-10-05 14:47:22 +0530

    [Soumya] Refactor: Validate Password 3

--- UserRegistration/UserRegistration/RegexClass.cs
+++ UserRegistration/UserRegistration/RegexClass.cs
@@ -11,7 +11,7 @@ namespace UserRegistration
         public static string LASTNAMEREGEX = "^[A-Z]{1}[A-Za-z]{2,}$";
         public static string EMAILREGEX = "^[0-9a-zA-Z]+[.+-_]{0,1}[0-9a-zA-Z]+[@][a-zA-Z]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2,3}){0,1}";
         public static string MOBILENUMBERREGEX = "^[9]{1}[1]{1}[ ]{1}[6-9]{1}[0-9]{9}$";
-        public static string PASSWORDREGEX = "^(?=.*?[A-Z])[A-Za-z0-9@$!%*#?&]{8,}$";
+        public static string PASSWORDREGEX = "^(?=.*?[A-Z])(?=.*?[0-9])[A-Za-z0-9@$!%*#?&]{8,}$";
         public bool validateFirstName(string name)
         {
             return Regex.IsMatch(name, FIRSTNAMEREGEX);
